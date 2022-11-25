fe69dbcc63347f5fa8906acbd825d52360b3949b
unknown <assegidassefa7@gmail.com>
2022-11-24 19:38:22 -0800

    creating factorial

---
+++ factorial/factorial.js
@@ -0,0 +1,14 @@
+const factorial = (n) => {
+  let fac = 1;
+  //   if (n == 0) {
+  //     return fac;
+  //   }
+  for (i = 1; i <= n; i++) {
+    fac *= i;
+  }
+  return fac;
+};
+
+const product = factorial(0);
+
+console.log(product);
