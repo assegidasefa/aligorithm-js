f4074dab884f66482302f7372d9be8502610af56
unknown <assegidassefa7@gmail.com>
2022-11-26 23:26:09 -0800

    adding bitwise Oprator for power of two checker

--- prime_num_check/isPowerOfTwo.js
+++ prime_num_check/isPowerOfTwo.js
@@ -1,20 +1,41 @@
-const isPowerOfTwo = (n) => {
+// const isPowerOfTwo = (n) => {
+//   if (n < 1) {
+//     return false;
+//   }
+//   while (n > 1) {
+//     if (n % 2 !== 0) {
+//       return false;
+//     }
+//     n = n / 2;
+//   }
+//   return true;
+// };
+
+// const result = isPowerOfTwo(5);
+// const resultOne = isPowerOfTwo(2);
+// const resultTwo = isPowerOfTwo(1);
+
+// console.log(result);
+// console.log(resultOne);
+// console.log(resultTwo);
+
+// isPowerOfTwo fun is big-o == 0(logn)
+
+// in bitwise operation
+
+const bitwiseOperation = (n) => {
   if (n < 1) {
     return false;
   }
-  while (n > 1) {
-    if (n % 2 !== 0) {
-      return false;
-    }
-    n = n / 2;
-  }
-  return true;
+  return (n & (n - 1)) === 0;
 };
 
-const result = isPowerOfTwo(5);
-const resultOne = isPowerOfTwo(2);
-const resultTwo = isPowerOfTwo(1);
+let outComeOne = bitwiseOperation(5);
+let outComeTwo = bitwiseOperation(2);
+let outComeThree = bitwiseOperation(1);
 
-console.log(result);
-console.log(resultOne);
-console.log(resultTwo);
+console.log(Boolean(outComeOne));
+console.log(Boolean(outComeTwo));
+console.log(Boolean(outComeThree));
+
+// big-o = o(1)
