e8840120ebe46205777c9865ac0eacfa84459e31
unknown <assegidassefa7@gmail.com>
2022-11-28 19:23:56 -0800

    adding recurrisive function for factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,16 +1,24 @@
-const factorial = (n) => {
-  let fac = 1;
-  //   if (n == 0) {
-  //     return fac;
-  //   }
-  for (i = 1; i <= n; i++) {
-    fac *= i;
-  }
-  return fac;
-};
+// const factorial = (n) => {
+//   let fac = 1;
+//   for (i = 1; i <= n; i++) {
+//     fac *= i;
+//   }
+//   return fac;
+// };
 
-const product = factorial(0);
+// const product = factorial(0);
 
-console.log(product);
+// console.log(product);
 
 // big-o = o(n)
+
+const recurssiveFactorial = (n) => {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recurssiveFactorial(n - 1);
+};
+
+console.log(recurssiveFactorial(5));
+console.log(recurssiveFactorial(0));
+console.log(recurssiveFactorial(1));
